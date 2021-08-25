c58d2e6bb188e88ce864cabb920e9e95cf7ee1c2
Julius Linus <juliuslinus1@example.com>
2021-08-24 23:00:01 -0500

    second commit

---
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+/** ORDER OF BUSINESS FOR TODAY
+ * -----------------------------------
+ * [BUG REPORT LISTS]
+ * [BUG REPORT] SecondActivity restarts timer after changing to landscape view
+ * [BUG REPORT] timer doesn't reset seconds when initial input in seconds is 00, using 01:00 test case
+ *
+ *
+ * [TODO LIST]
+ * Document code
+ * Move this all into github
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+    private int hour, minute, second;
+    private EditText hourInput, minuteInput, secondInput;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button setTimer = findViewById(R.id.setTimerBtn);
+        setTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gatherInput();
+                sendInput();
+            }
+        });
+
+    }
+
+    private void gatherInput()
+    {
+        hourInput = findViewById(R.id.hours);
+        minuteInput = findViewById(R.id.minutes);
+        secondInput = findViewById(R.id.seconds);
+
+        hour = Integer.parseInt(hourInput.getText().toString());
+        minute = Integer.parseInt(minuteInput.getText().toString());
+        second = Integer.parseInt(secondInput.getText().toString());
+    }
+
+    private void sendInput()
+    {
+        intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("HourInput", hour);
+        intent.putExtra("MinuteInput", minute);
+        intent.putExtra("SecondInput", second);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/timer/SecondActivity.java
@@ -0,0 +1,152 @@
+package com.example.timer;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class SecondActivity extends AppCompatActivity {
+    private final String TAG = "SecondActivity.java";
+    private int hours, minutes, seconds;
+    private boolean isPaused, isPressed;
+    private long timeRemaining; // I have the time remaining when the countdowntimer was canceled.
+    private CountDownTimer mTimer;
+    private TextView timer;
+    private Button resetBtn, stopBtn;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        timer = findViewById(R.id.timer);
+
+        gatherDataFromIntent();
+        countDown(userInputInMilli(hours, minutes, seconds));
+
+
+        resetBtn = findViewById(R.id.resetButton);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    intent = new Intent(SecondActivity.this, MainActivity.class);
+                    startActivity(intent);
+            }
+        });
+
+
+        stopBtn = findViewById(R.id.stopButton);
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isPressed)
+                {
+                    stopBtn.setBackgroundColor(ContextCompat.getColor(SecondActivity.this, R.color.lightergray));
+                    isPressed = !isPressed;
+
+                }
+                else
+                {
+                    stopBtn.setBackgroundColor(ContextCompat.getColor(SecondActivity.this, R.color.orange));
+                    isPressed = !isPressed;
+                }
+
+                if(!isPaused)
+                {
+                    mTimer.cancel();
+                    isPaused = !isPaused;
+                }
+                else
+                {
+                    countDown(timeRemaining);
+                    isPaused = !isPaused;
+                }
+
+
+            }
+        });
+
+    }
+
+    private void gatherDataFromIntent()
+    {
+        intent = getIntent();
+        hours = intent.getIntExtra("HourInput", 0);
+        minutes = intent.getIntExtra("MinuteInput", 0);
+        seconds = intent.getIntExtra("SecondInput", 0);
+
+    }
+
+    private void countDown(long milli)
+    {
+        mTimer = new CountDownTimer(milli, 1000)
+        {
+            public void onTick(long millisUntilFinished) //while loop messes with onTick
+            {
+                timeRemaining = millisUntilFinished; //updates every tick
+                formatTimer();
+                onTickUpdater();
+            }
+
+            public void onFinish()
+            {
+                timer.setText("00:00:00");
+            }
+        }.start();
+    }
+
+    private void formatTimer()
+    {
+        timer.setText(formatTimerHelper(hours) + ":" + formatTimerHelper(minutes) + ":" + formatTimerHelper(seconds));
+    }
+
+    private String formatTimerHelper(int time)
+    {
+        String formatted = "";
+        if(time >= 10)
+        {
+            formatted += time;
+        }
+        else
+        {
+            formatted += "0" + time;
+        }
+
+        return formatted;
+    }
+
+    private int userInputInMilli(int h, int m, int s)
+    {
+        int result = 0;
+        result += s * 1000;
+        result += m * 60000;
+        result += h * 3600000;
+        return result;
+    }
+
+    private void onTickUpdater()
+    {
+        seconds--;
+        if(seconds == 0 && minutes > 0)
+        {
+            minutes--;
+            seconds = 59;
+        }
+
+        if(minutes == 0 && hours > 0)
+        {
+            hours--;
+            minutes = 59;
+            seconds = 59;
+        }
+    }
+
+}
